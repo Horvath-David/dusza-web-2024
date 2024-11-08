a757e12f913947fa3a5ada22169c1745209e7c29
LoneHusko <vadmarton06@gmail.com>
2024-11-08 17:00:53 +0100

    added supplementary teammate field to team model and check field constraints on team creation

--- backend/team/response.py
+++ backend/team/response.py
@@ -7,7 +7,7 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_POST, require_http_methods
 
 from authenticate import wrappers
-from api.models import Team
+from api.models import Team, School, ProgrammingLanguage, Category
 
 
 @require_POST
@@ -22,11 +22,37 @@ def create_team(request: WSGIRequest):
             "error": "Invalid request body",
         }, status=400)
 
+    if len(body["supplementary_names"]) > 1 or len(body["supplementary_grades"]) > 1 or len(body["names"]) > 3 \
+        or len(body["grades"]) > 3:
+        return JsonResponse({
+            "status": "Error",
+            "error": "Invalid request body",
+        }, status=400)
+
+    if not School.objects.filter(id=body["school_id"]).exists():
+        return JsonResponse({
+            "status": "Error",
+            "error": f"School record with ID {body["school_id"]} does not exist",
+        }, status=400)
+
+    if not ProgrammingLanguage.objects.filter(id=body["prog_lang_id"]).exists():
+        return JsonResponse({
+            "status": "Error",
+            "error": f"Programming language record with ID {body["prog_lang_id"]} does not exist",
+        }, status=400)
+
+    if not Category.objects.filter(id=body["category_id"]).exists():
+        return JsonResponse({
+            "status": "Error",
+            "error": f"Category record with ID {body["category_id"]} does not exist",
+        }, status=400)
+
     Team.objects.create(
         name=body["team_name"],
         owner=request.user,
         school_id=body["school_id"],
         members=dict(zip(body["names"], body["grades"])),
+        supplementary_members=dict(zip(body["supplementary_names"], body["supplementary_grades"])),
         teacher_name=body["teacher_name"],
         category_id=body["category_id"],
         prog_lang_id=body["prog_lang_id"]
@@ -42,6 +68,12 @@ def create_team(request: WSGIRequest):
 @login_required
 @wrappers.require_role(["contestant"])
 def edit_team(request: WSGIRequest, team_id):
+    if Team.objects.get(id=team_id).owner != request.user:
+        return JsonResponse({
+            "status": "Error",
+            "error": "You are not the owner of this object",
+        }, status=403)
+
     if request.method == "DELETE":
         if not Team.objects.filter(id=team_id).exists():
             return JsonResponse({
