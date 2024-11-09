7b4b39f3a82939f3b3bc78898a92302dc56705c1
SledDev <sleddev@asd.dog>
2024-11-09 09:41:41 +0100

    team_id to extended user data

--- backend/me/utils.py
+++ backend/me/utils.py
@@ -1,11 +1,12 @@
 from typing import TypedDict
 from django.contrib.auth.models import User
 
-from api.models import UserData
+from api.models import Team, UserData
 
 class ExtendedUserData(TypedDict):
     user_id: int
     user_data_id: int
+    team_id: int | None
     username: str
     display_name: str
     role: str 
@@ -14,9 +15,11 @@ class ExtendedUserData(TypedDict):
 
 def get_extended_user_data(user: User) -> ExtendedUserData:
     user_data = UserData.objects.get(user=user)
+    team_id_obj = Team.objects.only("id").filter(owner=user).first()
     return {
         "user_id": user_data.user.pk,
         "user_data_id": user_data.pk,
+        "team_id": None if team_id_obj == None else team_id_obj.id,
         "username": user_data.user.username,
         "display_name": user_data.display_name,
         "role": user_data.role,
