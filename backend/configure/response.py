7284b368f53ea29aa9a2256b2856718e89b2074d
SledDev <sleddev@asd.dog>
2024-11-10 00:48:33 +0100

    what if we just fall_ im not going without you, and youre not going alone

--- backend/configure/response.py
+++ backend/configure/response.py
@@ -5,11 +5,28 @@ import datetime
 from django.contrib.auth.decorators import login_required
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import JsonResponse
-from django.views.decorators.http import  require_POST
+from django.views.decorators.http import  require_GET, require_POST
 
 from api.models import Config
 from authenticate import wrappers
 # Create your views here.
+
+@login_required
+@require_GET
+@wrappers.require_role(["organizer"])
+def get_registration_deadline(request: WSGIRequest):
+    config_obj = Config.objects.first(name="reg_deadline")
+    if config_obj == None:
+        return JsonResponse({
+            "status": "Error",
+            "error": "Nem található a kért konfigurációs objektum",
+        }, status= 404)
+
+    return JsonResponse({
+        "status": "Ok",
+        "error": None,
+        "deadline_date": config_obj.data["date"]
+    }, status= 200)
 
 
 @login_required
--- backend/configure/urls.py
+++ backend/configure/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import response
 
 urlpatterns = [
+    path("deadline/get", response.get_registration_deadline),
     path("deadline/set", response.set_registration_deadline)
 ]
