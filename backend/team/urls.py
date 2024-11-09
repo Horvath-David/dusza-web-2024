6a0386f0c3b7d2464d47e446b1f88b4ade85872d
LoneHusko <vadmarton06@gmail.com>
2024-11-09 16:27:58 +0100

    wadafak django

--- backend/team/urls.py
+++ backend/team/urls.py
@@ -4,9 +4,9 @@ from . import response
 urlpatterns = [
     path('create', response.create_team),
     path('<int:team_id>/manage', response.edit_team),
+    path('me', response.my_team),
     path('all', response.all_team),
     path('<int:team_id>/request_info_fix', response.request_info_fix),
     path('<int:team_id>/change_status/<str:status>', response.change_status),
-    path('<str:status>', response.get_by_status),
-    path('me', response.my_team),
+    path('get/<str:status>', response.get_by_status),
 ]
