4f800f06f5c7ba1e5065630940bb67d5469d0a1d
SledDev <sleddev@asd.dog>
2024-11-09 10:15:02 +0100

    removed IDOption, not necessary

--- frontend/src/pages/(app)/new-team.tsx
+++ frontend/src/pages/(app)/new-team.tsx
@@ -27,11 +27,6 @@ import {
 } from "~/components/ui/number-field";
 import { Label } from "~/components/ui/label";
 
-interface IDOption {
-  value: number;
-  label: string;
-}
-
 async function getProgLangs() {
   const res = await makeRequest<{
     status: string;
@@ -79,249 +74,262 @@ const NewTeam: Component<{}> = () => {
 
   const [teacher, setTeacher] = createSignal("");
 
-  const [allProgLang, setAllProgLang] = createSignal<IDOption[]>([]);
-  const [allCategory, setAllCategory] = createSignal<IDOption[]>([]);
-  const [allSchool, setAllSchool] = createSignal<IDOption[]>([]);
+  const [allSchool, setAllSchool] = createSignal<School[]>([]);
+  const [allCategory, setAllCategory] = createSignal<Category[]>([]);
+  const [allProgLang, setAllProgLang] = createSignal<ProgrammingLanguage[]>([]);
 
-  const [school, setSchool] = createSignal<IDOption>();
-  const [category, setCategory] = createSignal<IDOption>();
-  const [programmingLang, setProgrammingLang] = createSignal<IDOption>();
+  const [school, setSchool] = createSignal<School>();
+  const [category, setCategory] = createSignal<Category>();
+  const [programmingLang, setProgrammingLang] =
+    createSignal<ProgrammingLanguage>();
 
   onMount(async () => {
-    setAllSchool(
-      (await getSchool()).map((x) => ({
-        value: x.id,
-        label: x.name,
-      })),
-    );
-
-    setAllCategory(
-      (await getCategory()).map((x) => ({
-        value: x.id,
-        label: x.name,
-      })),
-    );
-
-    setAllProgLang(
-      (await getProgLangs()).map((x) => ({
-        value: x.id,
-        label: x.name,
-      })),
-    );
+    setAllSchool(await getSchool());
+    setAllCategory(await getCategory());
+    setAllProgLang(await getProgLangs());
   });
 
-    const handleSubmit =  async(event:SubmitEvent) => {
+  const handleSubmit = async (event: SubmitEvent) => {
     event.preventDefault();
     // console.log(school()?.value, category()?.value, programmingLang()?.value, teamMateOneName(), teamMateOneGrade().valueOf, teacher().valueOf, teamName().valueOf)
     const res = await makeRequest({
       method: "POST",
       endpoint: "/team/create",
       body: {
-        "team_name": teamName(),
-        "school_id": school()?.value,
-        "names": [teamMateOneName(), teamMateSecondName(), teamMateThirdName()],
-        "grades": [teamMateOneGrade(), teamMateSecondGrade(), teamMateThirdGrade()],
-        "supplementary_names": [substituteTeamMateName()],
-        "supplementary_grades": [substituteTeamMateGrade()],
-        "teacher_name": teacher(),
-        "category_id": category()?.value,
-        "prog_lang_id": programmingLang()?.value
+        team_name: teamName(),
+        school_id: school()?.id,
+        names: [teamMateOneName(), teamMateSecondName(), teamMateThirdName()],
+        grades: [
+          teamMateOneGrade(),
+          teamMateSecondGrade(),
+          teamMateThirdGrade(),
+        ],
+        supplementary_names: [substituteTeamMateName()],
+        supplementary_grades: [substituteTeamMateGrade()],
+        teacher_name: teacher(),
+        category_id: category()?.id,
+        prog_lang_id: programmingLang()?.id,
       },
     });
     console.log(res);
-  }
-
+  };
 
   return (
     <div class="mx-auto flex max-w-sm flex-col items-center gap-4">
       <h1 class="my-8 text-2xl font-semibold">Új csapat regisztrálása</h1>
       <form onSubmit={handleSubmit}>
-
-      <TextField class="max-w-full" value={teamName()} onChange={setTeamName} required>
-        <TextFieldLabel>Csapatnév:</TextFieldLabel>
-        <TextFieldInput type="text" name="teamNameInput"></TextFieldInput>
-      </TextField>
-
-      <h2 class="mb-4 mt-8 text-xl font-semibold">Csapattagok: </h2>
-
-      {/* Teammate #1 */}
-      <div class="flex gap-4">
-        <TextField value={teamMateOneName()} onChange={setTeamMateOneName} required>
-          <TextFieldLabel>Első csapattag neve: </TextFieldLabel>
-          <TextFieldInput type="text" />
-        </TextField>
-        <NumberField required
-          class="grid w-full max-w-xs flex-1 basis-24 items-center gap-1.5"
-          minValue={8}
-          maxValue={13}
-          value={teamMateOneGrade()}
-          onChange={setTeamMateOneGrade}
-        >
-          <NumberFieldLabel>Osztálya:</NumberFieldLabel>
-          <NumberFieldGroup>
-            <NumberFieldInput />
-            <NumberFieldIncrementTrigger />
-            <NumberFieldDecrementTrigger />
-          </NumberFieldGroup>
-        </NumberField>
-      </div>
-
-      {/* Teammate #2 */}
-      <div class="flex gap-4">
-        <TextField required
-          value={teamMateSecondName()}
-          onChange={setTeamMateSecondName}
-        >
-          <TextFieldLabel>Második csapattag neve: </TextFieldLabel>
-          <TextFieldInput type="text" />
-        </TextField>
-        <NumberField required={true}
-          class="grid w-full max-w-xs flex-1 basis-24 items-center gap-1.5"
-          minValue={8}
-          maxValue={13}
-          value={teamMateSecondGrade()}
-          onChange={setTeamMateSecondGrade}
-        >
-          <NumberFieldLabel>Osztálya:</NumberFieldLabel>
-          <NumberFieldGroup>
-            <NumberFieldInput />
-            <NumberFieldIncrementTrigger />
-            <NumberFieldDecrementTrigger />
-          </NumberFieldGroup>
-        </NumberField>
-      </div>
-
-      {/* Teammate #3 */}
-      <div class="flex gap-4">
-        <TextField value={teamMateThirdName()} onChange={setTeamMateThirdName} required={true}>
-          <TextFieldLabel>Harmadik csapattag neve: </TextFieldLabel>
-          <TextFieldInput type="text" />
-        </TextField>
-        <NumberField required
-          class="grid w-full max-w-xs flex-1 basis-24 items-center gap-1.5"
-          minValue={8}
-          maxValue={13}
-          value={teamMateThirdGrade()}
-          onChange={setTeamMateThirdGrade}
-        >
-          <NumberFieldLabel>Osztálya:</NumberFieldLabel>
-          <NumberFieldGroup>
-            <NumberFieldInput />
-            <NumberFieldIncrementTrigger />
-            <NumberFieldDecrementTrigger />
-          </NumberFieldGroup>
-        </NumberField>
-      </div>
-
-      {/* Substitute teammate */}
-      <div class="flex gap-4">
         <TextField
-          value={substituteTeamMateName()}
-          onChange={setSubstitudeTeamMateName}
+          class="max-w-full"
+          value={teamName()}
+          onChange={setTeamName}
+          required
         >
-          <TextFieldLabel>Pót csapattag neve: </TextFieldLabel>
+          <TextFieldLabel>Csapatnév:</TextFieldLabel>
+          <TextFieldInput type="text" name="teamNameInput"></TextFieldInput>
+        </TextField>
+
+        <h2 class="mb-4 mt-8 text-xl font-semibold">Csapattagok: </h2>
+
+        {/* Teammate #1 */}
+        <div class="flex gap-4">
+          <TextField
+            value={teamMateOneName()}
+            onChange={setTeamMateOneName}
+            required
+          >
+            <TextFieldLabel>Első csapattag neve: </TextFieldLabel>
+            <TextFieldInput type="text" />
+          </TextField>
+          <NumberField
+            required
+            class="grid w-full max-w-xs flex-1 basis-24 items-center gap-1.5"
+            minValue={8}
+            maxValue={13}
+            value={teamMateOneGrade()}
+            onChange={setTeamMateOneGrade}
+          >
+            <NumberFieldLabel>Osztálya:</NumberFieldLabel>
+            <NumberFieldGroup>
+              <NumberFieldInput />
+              <NumberFieldIncrementTrigger />
+              <NumberFieldDecrementTrigger />
+            </NumberFieldGroup>
+          </NumberField>
+        </div>
+
+        {/* Teammate #2 */}
+        <div class="flex gap-4">
+          <TextField
+            required
+            value={teamMateSecondName()}
+            onChange={setTeamMateSecondName}
+          >
+            <TextFieldLabel>Második csapattag neve: </TextFieldLabel>
+            <TextFieldInput type="text" />
+          </TextField>
+          <NumberField
+            required={true}
+            class="grid w-full max-w-xs flex-1 basis-24 items-center gap-1.5"
+            minValue={8}
+            maxValue={13}
+            value={teamMateSecondGrade()}
+            onChange={setTeamMateSecondGrade}
+          >
+            <NumberFieldLabel>Osztálya:</NumberFieldLabel>
+            <NumberFieldGroup>
+              <NumberFieldInput />
+              <NumberFieldIncrementTrigger />
+              <NumberFieldDecrementTrigger />
+            </NumberFieldGroup>
+          </NumberField>
+        </div>
+
+        {/* Teammate #3 */}
+        <div class="flex gap-4">
+          <TextField
+            value={teamMateThirdName()}
+            onChange={setTeamMateThirdName}
+            required={true}
+          >
+            <TextFieldLabel>Harmadik csapattag neve: </TextFieldLabel>
+            <TextFieldInput type="text" />
+          </TextField>
+          <NumberField
+            required
+            class="grid w-full max-w-xs flex-1 basis-24 items-center gap-1.5"
+            minValue={8}
+            maxValue={13}
+            value={teamMateThirdGrade()}
+            onChange={setTeamMateThirdGrade}
+          >
+            <NumberFieldLabel>Osztálya:</NumberFieldLabel>
+            <NumberFieldGroup>
+              <NumberFieldInput />
+              <NumberFieldIncrementTrigger />
+              <NumberFieldDecrementTrigger />
+            </NumberFieldGroup>
+          </NumberField>
+        </div>
+
+        {/* Substitute teammate */}
+        <div class="flex gap-4">
+          <TextField
+            value={substituteTeamMateName()}
+            onChange={setSubstitudeTeamMateName}
+          >
+            <TextFieldLabel>Pót csapattag neve: </TextFieldLabel>
+            <TextFieldInput type="text" />
+          </TextField>
+          <NumberField
+            class="grid w-full max-w-xs flex-1 basis-24 items-center gap-1.5"
+            minValue={8}
+            maxValue={13}
+            value={substituteTeamMateGrade()}
+            onChange={setSubstitudeTeamMateGrade}
+          >
+            <NumberFieldLabel>Osztálya:</NumberFieldLabel>
+            <NumberFieldGroup>
+              <NumberFieldInput />
+              <NumberFieldIncrementTrigger />
+              <NumberFieldDecrementTrigger />
+            </NumberFieldGroup>
+          </NumberField>
+        </div>
+
+        <h2 class="mb-4 mt-8 text-xl font-semibold">További adatok: </h2>
+
+        <TextField
+          required
+          class="max-w-full"
+          value={teacher()}
+          onChange={setTeacher}
+        >
+          <TextFieldLabel>Felkészítő tanár neve: </TextFieldLabel>
           <TextFieldInput type="text" />
         </TextField>
-        <NumberField
-          class="grid w-full max-w-xs flex-1 basis-24 items-center gap-1.5"
-          minValue={8}
-          maxValue={13}
-          value={substituteTeamMateGrade()}
-          onChange={setSubstitudeTeamMateGrade}
+
+        {/* School selection */}
+        <Combobox<School>
+          required
+          options={allSchool()}
+          optionValue="id"
+          optionTextValue="name"
+          optionLabel="name"
+          placeholder="Válassz iskolát..."
+          class="w-full"
+          value={school()}
+          onChange={(val) => setSchool(val || undefined)}
+          itemComponent={(props) => (
+            <ComboboxItem item={props.item}>
+              <ComboboxItemLabel>{props.item.rawValue.name}</ComboboxItemLabel>
+              <ComboboxItemIndicator />
+            </ComboboxItem>
+          )}
         >
-          <NumberFieldLabel>Osztálya:</NumberFieldLabel>
-          <NumberFieldGroup>
-            <NumberFieldInput />
-            <NumberFieldIncrementTrigger />
-            <NumberFieldDecrementTrigger />
-          </NumberFieldGroup>
-        </NumberField>
-      </div>
+          <Label class="mb-1.5 block">Csapat iskolája:</Label>
+          <ComboboxControl>
+            <ComboboxInput />
+            <ComboboxTrigger />
+          </ComboboxControl>
+          <ComboboxContent />
+        </Combobox>
 
-      <h2 class="mb-4 mt-8 text-xl font-semibold">További adatok: </h2>
+        {/* Category selection */}
+        <Combobox<Category>
+          required
+          options={allCategory()}
+          optionValue="id"
+          optionTextValue="name"
+          optionLabel="name"
+          placeholder="Válassz kategóriát..."
+          class="w-full"
+          value={category()}
+          onChange={(val) => setCategory(val || undefined)}
+          itemComponent={(props) => (
+            <ComboboxItem item={props.item}>
+              <ComboboxItemLabel>{props.item.rawValue.name}</ComboboxItemLabel>
+              <ComboboxItemIndicator />
+            </ComboboxItem>
+          )}
+        >
+          <Label class="mb-1.5 block">Csapat kategóriája:</Label>
+          <ComboboxControl>
+            <ComboboxInput />
+            <ComboboxTrigger />
+          </ComboboxControl>
+          <ComboboxContent />
+        </Combobox>
 
-      <TextField required class="max-w-full" value={teacher()} onChange={setTeacher}>
-        <TextFieldLabel>Felkészítő tanár neve: </TextFieldLabel>
-        <TextFieldInput type="text" />
-      </TextField>
+        {/* Language selection */}
+        <Combobox<ProgrammingLanguage>
+          required
+          options={allProgLang()}
+          optionValue="id"
+          optionTextValue="name"
+          optionLabel="name"
+          placeholder="Válassz programnyelvet..."
+          class="w-full"
+          value={programmingLang()}
+          onChange={(val) => setProgrammingLang(val || undefined)}
+          itemComponent={(props) => (
+            <ComboboxItem item={props.item}>
+              <ComboboxItemLabel>{props.item.rawValue.name}</ComboboxItemLabel>
+              <ComboboxItemIndicator />
+            </ComboboxItem>
+          )}
+        >
+          <Label class="mb-1.5 block">Csapat programnyelve:</Label>
+          <ComboboxControl>
+            <ComboboxInput />
+            <ComboboxTrigger />
+          </ComboboxControl>
+          <ComboboxContent />
+        </Combobox>
 
-      {/* School selection */}
-      <Combobox<IDOption> required
-        options={allSchool()}
-        optionValue="value"
-        optionTextValue="label"
-        optionLabel="label"
-        placeholder="Válassz iskolát..."
-        class="w-full"
-        value={school()}
-        onChange={(val) => setSchool(val || undefined)}
-        itemComponent={(props) => (
-          <ComboboxItem item={props.item}>
-            <ComboboxItemLabel>{props.item.rawValue.label}</ComboboxItemLabel>
-            <ComboboxItemIndicator />
-          </ComboboxItem>
-        )}
-      >
-        <Label class="mb-1.5 block">Csapat iskolája:</Label>
-        <ComboboxControl>
-          <ComboboxInput />
-          <ComboboxTrigger />
-        </ComboboxControl>
-        <ComboboxContent />
-      </Combobox>
-
-      {/* Category selection */}
-      <Combobox<IDOption> required
-        options={allCategory()}
-        optionValue="value"
-        optionTextValue="label"
-        optionLabel="label"
-        placeholder="Válassz kategóriát..."
-        class="w-full"
-        value={category()}
-        onChange={(val) => setCategory(val || undefined)}
-        itemComponent={(props) => (
-          <ComboboxItem item={props.item}>
-            <ComboboxItemLabel>{props.item.rawValue.label}</ComboboxItemLabel>
-            <ComboboxItemIndicator />
-          </ComboboxItem>
-        )}
-      >
-        <Label class="mb-1.5 block">Csapat kategóriája:</Label>
-        <ComboboxControl>
-          <ComboboxInput />
-          <ComboboxTrigger />
-        </ComboboxControl>
-        <ComboboxContent />
-      </Combobox>
-
-      {/* Language selection */}
-      <Combobox<IDOption> required
-        options={allProgLang()}
-        optionValue="value"
-        optionTextValue="label"
-        optionLabel="label"
-        placeholder="Válassz programnyelvet..."
-        class="w-full"
-        value={programmingLang()}
-        onChange={(val) => setProgrammingLang(val || undefined)}
-        itemComponent={(props) => (
-          <ComboboxItem item={props.item}>
-            <ComboboxItemLabel>{props.item.rawValue.label}</ComboboxItemLabel>
-            <ComboboxItemIndicator />
-          </ComboboxItem>
-        )}
-      >
-        <Label class="mb-1.5 block">Csapat programnyelve:</Label>
-        <ComboboxControl>
-          <ComboboxInput />
-          <ComboboxTrigger />
-        </ComboboxControl>
-        <ComboboxContent />
-      </Combobox>
-
-      <Button class="mb-4 mt-6 w-full" type="submit">Beadás</Button>
-     </form>
+        <Button class="mb-4 mt-6 w-full" type="submit">
+          Beadás
+        </Button>
+      </form>
     </div>
   );
 };
